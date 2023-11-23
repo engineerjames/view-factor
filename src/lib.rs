d9153b09b58d0b1f2c3b8f2578439dd0a4700bab
James Armes <jamesleearmes@gmail.com>
2023-11-22 23:58:53 -0700

    Refactor to calculate length and angle on the fly

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@ pub struct Point2D<T: num::Float> {
 
 #[derive(Debug)]
 pub struct Line2D<T: num::Float> {
-    pub points: [Point2D<T>; 2],
+    points: [Point2D<T>; 2],
+    angle: T,
+    length: T,
 }
 
 impl<T> Point2D<T>
@@ -24,15 +26,19 @@ impl<T> Line2D<T>
 where
     T: Float,
 {
-    pub fn length(&self) -> T {
-        return (((self.points[0].x - self.points[1].x) * (self.points[0].x - self.points[1].x))
-            + ((self.points[0].y - self.points[1].y) * (self.points[0].y - self.points[1].y)))
-            .sqrt();
+    pub fn get_angle(&self) -> T {
+        return self.angle;
+    }
+
+    pub fn get_length(&self) -> T {
+        return self.length;
     }
 
     pub fn new((x1, y1): (T, T), (x2, y2): (T, T)) -> Line2D<T> {
         Line2D {
             points: [Point2D::new((x1, y1)), Point2D::new((x2, y2))],
+            angle: T::atan2(y2 - y1, x2 - x1),
+            length: (((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))).sqrt(),
         }
     }
 }
@@ -44,34 +50,18 @@ mod tests {
     const EPSILON: f32 = 1e-10;
 
     #[test]
-    fn test_length_calculation() {
-        let test_line = Line2D {
-            points: [Point2D { x: 0.0, y: -0.3 }, Point2D { x: 0.1, y: 0.2 }],
-        };
-
-        assert!((test_line.length() - 0.50990194).abs() < EPSILON);
-    }
-
-    #[test]
-    fn test_equal_points_have_zero_length() {
-        let test_line = Line2D {
-            points: [Point2D { x: 0.0, y: -0.3 }, Point2D { x: 0.0, y: -0.3 }],
-        };
-
-        assert!((test_line.length()) < EPSILON);
-    }
-
-    #[test]
     fn test_length_calculation_alternative_construction() {
         let test_line = Line2D::new((0.0, -0.3), (0.1, 0.2));
 
-        assert!((test_line.length() - 0.50990194).abs() < EPSILON);
+        assert_eq!(test_line.get_angle(), 0.0);
+
+        assert!((test_line.get_length() - 0.50990194).abs() < EPSILON);
     }
 
     #[test]
     fn test_equal_points_have_zero_length_alternative_construction() {
         let test_line = Line2D::new((0.0, -0.3), (0.0, -0.3));
 
-        assert!((test_line.length()) < EPSILON);
+        assert!((test_line.get_length()) < EPSILON);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,5 @@ use view_factor::*;
 // Just to going to start by hard-coding everything
 fn main() {
     // Units are in meters
-    let line1 = Line2D {
-        points: [Point2D { x: 0.0, y: 0.0 }, Point2D { x: 0.0, y: 1.0 }],
-    };
-
-    let line2 = Line2D {
-        points: [Point2D { x: 0.5, y: 0.0 }, Point2D { x: 1.5, y: 0.0 }],
-    };
-
-    println!("{:?}", line1);
-    println!("{:?}", line2);
-
-    println!("{}", line1.length());
+    println!("Hello world");
 }
