8cc81533338ebc36928a56e1316b2259cdccb272
James Armes <jamesleearmes@gmail.com>
2023-12-17 11:57:01 -0700

    Update docs for dot product method

--- python/check_math.py
+++ python/check_math.py
@@ -30,9 +30,13 @@ y_segments = [[2, 4], [7, 4]]
 
 x1 = x_segments[0][0]
 x2 = x_segments[0][1]
+x3 = x_segments[1][0]
+x4 = x_segments[1][1]
 
 y1 = y_segments[0][0]
 y2 = y_segments[0][1]
+y3 = y_segments[1][0]
+y4 = y_segments[1][1]
 
 # Plot each line segment
 for x_seg, y_seg in zip(x_segments, y_segments):
@@ -42,13 +46,23 @@ for x_seg, y_seg in zip(x_segments, y_segments):
 dx = x2 - x1
 dy = y2 - y1
 
+dx2 = x4 - x3
+dy2 = y4 - y3
+
 midpoint = [(x1 + x2) / 2.0, (y1 + y2) / 2.0]
+midpoint2 = [(x3 + x4) / 2.0, (y3 + y4) / 2.0]
 
 norm_1 = [-dy, dx]
 norm_2 = [dy, -dx]
 
+norm_3 = [-dy2, dx2]
+norm_4 = [dy2, -dx2]
+
 plt.plot(norm_1[0]+midpoint[0], norm_1[1]+midpoint[1], marker='o', color='red')
 plt.plot(norm_2[0]+midpoint[0], norm_2[1]+midpoint[1], marker='o', color='green')
+plt.plot(norm_3[0]+midpoint2[0], norm_3[1]+midpoint2[1], marker='o', color='blue')
+plt.plot(norm_4[0]+midpoint2[0], norm_4[1]+midpoint2[1], marker='o', color='yellow')
+
 plt.plot(0, 0, marker='o', color='black')
 
 second_line_pt1 = [x_segments[1][0], y_segments[1][0]]
@@ -60,17 +74,12 @@ if do_segments_intersect([x_segments[0][0], x_segments[0][1], x_segments[1][0], 
 else:
     print("LINES DO NOT INTERSECT")
 
-# Calculate dot product between the normal vector and the other lines
-print(np.dot(norm_1, second_line_pt1))
-print(np.dot(norm_1, second_line_pt2))
-print(np.dot(norm_2, second_line_pt1))
-print(np.dot(norm_2, second_line_pt2))
-
-if (np.sum([np.dot(norm_1, second_line_pt1), np.dot(norm_1, second_line_pt2)]) >
-    np.sum([np.dot(norm_2, second_line_pt1), np.dot(norm_2, second_line_pt2)])):
-    print('Norm1 [red] is the right direction')
-else:
-    print('Norm2 [green] is the right direction.')
+# This works way better--we just need to find the combo of normals that
+# face each other (>0) and disregard the pairs that don't (<0)
+print(f'dot (blue.red)={np.dot(norm_3, norm_2)}')
+print(f'dot (green.red)={np.dot(norm_2, norm_4)}')
+print(f'dot (green.yellow)={np.dot(norm_2, norm_4)}')
+print(f'dot (yellow.blue)={np.dot(norm_4, norm_3)}')
 
 # Set axis labels and title
 plt.xlabel('X-axis')
